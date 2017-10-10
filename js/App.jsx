d8b6c82f10bf6ca771d559b6ddb63868099d3b6b
Nicholas Pappagalo <npappagalo@gmail.com>
2017-10-09 20:01:52 -0700

    add details page

--- js/App.jsx
+++ js/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { shape } from 'prop-types';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
+import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -11,10 +14,21 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route
+          path="/details/:id"
+          component={props => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} />;
+          }}
+        />
         <Route component={FourOhFour} />
       </Switch>
     </div>
   </BrowserRouter>
 );
+
+App.propTypes = {
+  match: shape.isRequired
+};
 
 export default App;
